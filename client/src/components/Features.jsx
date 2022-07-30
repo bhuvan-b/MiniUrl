9f4cb8c265ee7d64c444e4f470c7f86ab41c89d8
bhuvan-b <bokkabhuvan2@gmail.com>
2022-07-30 13:08:33 +0530

    Added Landing Page with Navbar and Footer

---
+++ client/src/components/Features.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Features = () => {
+  return (
+    <div className="py-10 my-4 max-w-[1280px] mx-auto">
+      <div className="container py-5">
+        <h3>Features</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Features;
