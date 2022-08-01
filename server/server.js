565aa2ced9f2fe1caa629486d94b8a71425fe08f
bhuvan-b <bokkabhuvan2@gmail.com>
2022-08-01 12:10:37 +0530

    Database Model and Generate Route added

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
-
 app.use(express.json());
 
 const connectDB = require("./config/db");
 connectDB();
 
 const PORT = process.env.port || 5000;
+
+app.use("/api/genShortUrl", require("./routes/genShortUrl"));
+
+app.use("/", (req, res) => res.json("Server Running"));
 
 app.listen(PORT, () => {
   console.log(`Server is running on Port: ${PORT}`);
