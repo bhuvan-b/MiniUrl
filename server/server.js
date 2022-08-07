17db8233795043a8150ae91aae42710b79b2e96f
bhuvan-b <bokkabhuvan2@gmail.com>
2022-08-07 16:29:30 +0530

    Minor Changes

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ app.use(cors());
 const connectDB = require("./config/db");
 connectDB();
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use("/api/genShortUrl", require("./routes/genShortUrl"));
 
